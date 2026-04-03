4a9f1b608b98f5cdb7417c0a3addd4de1e4c1834
teamzentrix <team.zentrixinfotech@gmail.com>
2026-04-03 10:56:55 +0530

    ok

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,9 @@ import Hero from './components/Hero';
 import AboutSection from './components/About';
 import Services from './components/Services';
 import QuickServices from './components/QuickServices';
+import TestimonialsPage from './Testimonials/page';
+import TestimonialsSection from './components/Testimonal';
+import Footer from './components/Footer';
 
 const page = () => {
   return (
@@ -12,6 +15,8 @@ const page = () => {
       <AboutSection/>
       <Services/>
       <QuickServices/>
+      <TestimonialsSection/>
+      <Footer/>
       
     </div>
   );
